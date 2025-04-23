499b04df30fc0319aaea0b462e6dadf97ade26c1
Jerpism <jere.m@live.fi>
2025-04-23 20:19:38 +0300

    add disable and enable irq to common.h

--- include/common.h
+++ include/common.h
@@ -9,4 +9,12 @@ static inline void spin(volatile uint32_t c){
     while(c--)(void)0;
 }
 
+static inline void disable_irq(){
+    asm volatile("cpsid if");
+}
+
+static inline void enable_irq(){
+    asm volatile("cpsie if");
+}
+
 #endif /* COMMON_H_ */
