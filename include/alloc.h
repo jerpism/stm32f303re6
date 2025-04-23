b7be43c6bded1f9582fab34588547ad6aeaa23f2
Jerpism <jere.m@live.fi>
2025-04-23 20:20:10 +0300

    Add very crude linear allocator for testing purposes

---
+++ include/alloc.h
@@ -0,0 +1,6 @@
+#ifndef ALLOC_H_
+#define ALLOC_H_
+
+void *malloc(size_t n);
+
+#endif /* ALLOC_H_ */
---
+++ src/alloc.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+/* Heap size in words */
+#define HEAPSIZE 4096 
+
+/* Crude allocator just to have dynamic allocation available 
+ * shouldn't really be used for anything */
+__attribute__((section(".heap"))) static uint32_t _heap[HEAPSIZE];
+
+static const uint8_t * const start_heap = (const uint8_t *)&_heap;
+static const uint8_t * const end_heap = (const uint8_t *)&_heap[HEAPSIZE-1];
+
+static uint8_t *heap_ptr = (uint8_t*)start_heap;
+
+static inline void *sbrk(size_t n){
+    uint8_t *base;
+
+    base = heap_ptr;
+    heap_ptr += n;
+
+    return (void *)base;
+
+}
+
+void *malloc(size_t n){
+    return sbrk(n);
+}
